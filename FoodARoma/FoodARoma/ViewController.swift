a5e0cc6fd15f165008846b5f87e43c951d9fc440
Alan S Mathew <alansmathew@icloud.com>
2023-06-04 21:52:57 -0400

    Launch screen design

---
+++ FoodARoma/FoodARoma/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodARoma
+//
+//  Created by Alan S Mathew on 2023-06-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FoodARoma/FoodARomaUITests/FoodARomaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FoodARomaUITestsLaunchTests.swift
+//  FoodARomaUITests
+//
+//  Created by Alan S Mathew on 2023-06-04.
+//
+
+import XCTest
+
+class FoodARomaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
