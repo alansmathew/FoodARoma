907e6d3f5f9a7d2e09de76c9b5c5e446e4e064b8
Alan S Mathew <43809019+alansmathew@users.noreply.github.com>
2023-07-19 15:34:47 -0400

    ar collection view done

--- FoodARoma/FoodARoma/Controller/BuildOwn/BuildOwnViewController.swift
+++ FoodARoma/FoodARoma/Controller/BuildOwn/BuildOwnViewController.swift
@@ -8,64 +8,156 @@
 import UIKit
 import RealityKit
 import ARKit
+import Lottie
 
 class BuildOwnViewController: UIViewController {
 
-    @IBOutlet weak var arView: ARView!
+    @IBOutlet weak var topingsCollectionView: UICollectionView!
+    @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var animation_lottie: LottieAnimationView!
+    
+    let topingNames = ["Size", "Mozzarella", "Cheddar", "Fontina" , "Chichen" ,"Beef", "Pepperoni", "Black olives", "Mushroom" ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        arView.session.delegate = self
         
-        setupARView()
-        
-        arView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap(recognizer:))))
-    }
-    
-    func setupARView() {
-        arView.automaticallyConfigureSession = false
+        sceneView.delegate = self
         let configuration = ARWorldTrackingConfiguration()
-        configuration.planeDetection = [.horizontal,.vertical]
-        configuration.environmentTexturing = .automatic
-        arView.session.run(configuration)
-    }
-    
-    @objc
-    func handleTap(recognizer: UITapGestureRecognizer) {
-        print("happpenes")
-        let location = recognizer.location(in: arView)
-        let results = arView.raycast(from: location, allowing: .estimatedPlane, alignment:.horizontal)
-        print(results)
-        if let firstResult = results.first {
-            let anchor = ARAnchor (name: "title1", transform: firstResult.worldTransform)
-            print(anchor)
-            arView.session.add(anchor: anchor)
-        }
-        else {
-            print("Object placement failed - couldn't find surface.")
-        }
-    }
-    
-    func placeObject(named entityName: String, for anchor : ARAnchor){
-        print(anchor)
-        let entity = try! ModelEntity.load(named: entityName)
+        configuration.planeDetection = .horizontal
+        sceneView.session.run(configuration)
         
-        entity.generateCollisionShapes(recursive: true)
-//        arView.installGestures([.rotation,.translation], for: entity as! Entity & HasCollision)
+        animation_lottie.isHidden = false
+        animation_lottie.animation = LottieAnimation.named("move.json")
+        animation_lottie.animationSpeed = 0.9
+        animation_lottie.loopMode = .loop
+        animation_lottie.play()
         
-        let anchorEntity = AnchorEntity(anchor: anchor)
-        anchorEntity.addChild(entity)
-        arView.scene.addAnchor((anchorEntity))
+        topingsCollectionView.dataSource = self
+        topingsCollectionView.delegate = self
+        topingsCollectionView.isHidden = true
     }
+
 
 }
 
-extension BuildOwnViewController: ARSessionDelegate {
-    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
-        for anchor in anchors {
-            if let anchorName = anchor.name, anchorName == "title1" {
-                placeObject(named: anchorName, for: anchor)
-            }
+extension BuildOwnViewController: ARSCNViewDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+
+        let planeNode = createPlaneNode(anchor: planeAnchor)
+        node.addChildNode(planeNode)
+        
+        DispatchQueue.main.async {
+            self.animation_lottie.stop()
+            self.animation_lottie.isHidden = true
         }
     }
+    
+    private func createPlaneNode(anchor: ARPlaneAnchor) -> SCNNode {
+        
+        
+        // Create a transparent plane with visible borders
+        let plane = SCNPlane(width: CGFloat(anchor.extent.x), height: CGFloat(anchor.extent.z))
+        plane.materials.first?.diffuse.contents = UIColor.clear
+        plane.materials.first?.isDoubleSided = true // Show both sides of the plane
+
+        let planeNode = SCNNode(geometry: plane)
+        planeNode.position = SCNVector3(anchor.center.x, 0, anchor.center.z)
+        planeNode.eulerAngles.x = -.pi / 2 // Rotate the plane to be horizontal
+
+        // Add a border using a plane with a visible color
+        let border = SCNPlane(width: CGFloat(anchor.extent.x), height: CGFloat(anchor.extent.z))
+        border.materials.first?.diffuse.contents = UIColor(red: 1.00, green: 0.61, blue: 0.00, alpha: 0.75)
+        border.materials.first?.isDoubleSided = true
+
+        let borderNode = SCNNode(geometry: border)
+        borderNode.position = SCNVector3(anchor.center.x, 0, anchor.center.z)
+        borderNode.eulerAngles.x = -.pi / 2 // Rotate the border plane to be horizontal
+
+        // Combine the transparent plane and the border plane in a single node
+        let planeContainerNode = SCNNode()
+        planeContainerNode.addChildNode(planeNode)
+        planeContainerNode.addChildNode(borderNode)
+
+//        return planeContainerNode
+        
+        
+        let scaleUpAction = SCNAction.scale(to: 1.2, duration: 0.5)
+        let scaleDownAction = SCNAction.scale(to: 1.0, duration: 0.5)
+        let pulsateAction = SCNAction.sequence([scaleUpAction, scaleDownAction])
+        let pulsateForeverAction = SCNAction.repeatForever(pulsateAction)
+
+        planeContainerNode.runAction(pulsateForeverAction)
+        planeContainerNode.name = "planeNode"
+
+        return planeContainerNode
+        
+    }
+}
+
+extension BuildOwnViewController {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first, let result = sceneView.hitTest(touch.location(in: sceneView), types: [.existingPlaneUsingExtent]).first else { return }
+
+        let position = SCNVector3(result.worldTransform.columns.3.x, result.worldTransform.columns.3.y, result.worldTransform.columns.3.z)
+        let objectNode = createYour3DObject() // Create your 3D object node here
+        
+        objectNode.position = position
+        sceneView.scene.rootNode.addChildNode(objectNode)
+        
+        DispatchQueue.main.async {
+            self.topingsCollectionView.isHidden = false
+        }
+        
+        sceneView.scene.rootNode.enumerateChildNodes { (node, _) in
+            print(node.name)
+              if node.name == "planeNode" {
+                  node.removeFromParentNode()
+              }
+          }
+    }
+    
+    private func createYour3DObject() -> SCNNode {
+        
+        
+        // Load the USDZ model
+        let url = Bundle.main.url(forResource: "title1", withExtension: "usdz")
+        let scene = try? SCNScene(url: url!)
+        let objectNode = scene!.rootNode.childNodes.first
+        return objectNode!
+        
+    }
+}
+
+extension BuildOwnViewController : UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return topingNames.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = topingsCollectionView.dequeueReusableCell(withReuseIdentifier: "topingsCellIdentifier", for: indexPath) as! PizzaTopingsCustomCollectionViewCell
+        cell.topingsNameLabel.text = topingNames[indexPath.row]
+        cell.topingsImage.image = UIImage(named: topingNames[indexPath.row])
+        return cell
+    }
+    
+
+}
+
+extension BuildOwnViewController : UICollectionViewDelegate{
+    
+}
+
+
+class PizzaTopingsCustomCollectionViewCell : UICollectionViewCell {
+    
+    @IBOutlet weak var contentViewBackground: UIView!
+    @IBOutlet weak var topingsImage: UIImageView!
+    @IBOutlet weak var topingsNameLabel: UILabel!
+    override func awakeFromNib() {
+        contentViewBackground.layer.cornerRadius = 10
+        contentViewBackground.layer.borderWidth = 1
+        contentViewBackground.layer.borderColor = UIColor.orange.cgColor
+    }
+    
 }
--- FoodARoma/FoodARoma/Controller/HomeVIew/SearchViewController.swift
+++ FoodARoma/FoodARoma/Controller/HomeVIew/SearchViewController.swift
@@ -20,6 +20,7 @@ class SearchViewController: UIViewController {
     @IBOutlet weak var search: UISearchBar!
     
     var commingPlatform = "Search"
+    private let refreshControl = UIRefreshControl()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,6 +37,9 @@ class SearchViewController: UIViewController {
         print(searchMenuData?.count)
         
         headingLAbel.text = commingPlatform
+        
+        refreshControl.addTarget(self, action: #selector(refreshData), for: .valueChanged)
+        searchTableView.refreshControl = refreshControl
 
         
     }
@@ -66,6 +70,49 @@ class SearchViewController: UIViewController {
           }
         }
     }
+    
+    func showDeleteWarning(for indexPath: IndexPath) {
+
+        let alert = UIAlertController(title: "Delete Menu ?", message: "Deleteion of a menu will cause issue while orderig. if there is anyone trying to order this menu at this time of deletion might still get through.", preferredStyle: .actionSheet)
+
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+
+        let deleteAction = UIAlertAction(title: "Delete", style: .destructive) { _ in
+            DispatchQueue.main.async {
+                
+                if self.search.text?.count ?? 0 > 1 {
+                    let x = self.searchMenuData![indexPath.row]
+                    if let menudata = self.AllMenuData {
+                        var count = 0
+                        for y in menudata {
+                            if y.menu_Name == x.menu_Name {
+                                self.AllMenuData?.remove(at: count)
+                                self.searchMenuData = self.AllMenuData
+                                self.search.text = ""
+//                                self.searchTableView.reloadData()
+                                self.searchTableView.deleteRows(at: [indexPath], with: .fade)
+                            }
+                            count += 1
+                        }
+                    }
+                }
+                else{
+                    self.AllMenuData?.remove(at: indexPath.row)
+                    self.searchMenuData = self.AllMenuData
+//                    self.searchTableView.reloadData()
+                    self.searchTableView.deleteRows(at: [indexPath], with: .fade)
+                    self.search.text = ""
+                }
+            }
+        }
+
+        //Add the actions to the alert controller
+        alert.addAction(cancelAction)
+        alert.addAction(deleteAction)
+
+        //Present the alert controller
+        present(alert, animated: true, completion: nil)
+    }
 }
 
 extension SearchViewController : UITableViewDataSource{
@@ -91,39 +138,54 @@ extension SearchViewController : UITableViewDataSource{
 }
 
 extension SearchViewController : UITableViewDelegate{
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let storyboard = UIStoryboard(name: "HomeOrder", bundle: nil)
         let viewC = storyboard.instantiateViewController(withIdentifier: "OrderDetailsViewController") as! OrderDetailsViewController
         viewC.SelectedOrder = searchMenuData![indexPath.row]
         navigationController?.pushViewController(viewC, animated: true)
     }
+    
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-            let action = UIContextualAction(style: .destructive, title: "Delete") { (action, view, completionHandler) in
-                
-                if self.search.text?.count ?? 0 > 1 {
-                    let x = self.searchMenuData![indexPath.row]
-                    if let menudata = self.AllMenuData {
-                        var count = 0
-                        for y in menudata {
-                            if y.menu_Name == x.menu_Name {
-                                self.AllMenuData?.remove(at: count)
-                                self.searchMenuData = self.AllMenuData
-                                self.search.text = ""
-                                self.searchTableView.reloadData()
-                            }
-                            count += 1
-                        }
-                    }
-                }
-                else{
-                    self.AllMenuData?.remove(at: indexPath.row)
-                    self.searchMenuData = self.AllMenuData
-                    self.searchTableView.reloadData()
-                    self.search.text = ""
-                }
-            }
-            return UISwipeActionsConfiguration(actions: [action])
-        }
+        let modifyAction = UIContextualAction(style: .normal, title:  "Delete", handler: { (ac:UIContextualAction, view:UIView, success:(Bool) -> Void) in
+             DispatchQueue.main.async {
+                 self.showDeleteWarning(for: indexPath)
+             }
+
+             success(true)
+         })
+
+         modifyAction.image = UIImage(named: "delete")
+         modifyAction.backgroundColor = .red
+
+         return UISwipeActionsConfiguration(actions: [modifyAction])
+        
+//        let action = UIContextualAction(style: .destructive, title: "Delete") { (action, view, completionHandler) in
+            
+//            if self.search.text?.count ?? 0 > 1 {
+//                let x = self.searchMenuData![indexPath.row]
+//                if let menudata = self.AllMenuData {
+//                    var count = 0
+//                    for y in menudata {
+//                        if y.menu_Name == x.menu_Name {
+//                            self.AllMenuData?.remove(at: count)
+//                            self.searchMenuData = self.AllMenuData
+//                            self.search.text = ""
+//                            self.searchTableView.reloadData()
+//                        }
+//                        count += 1
+//                    }
+//                }
+//            }
+//            else{
+//                self.AllMenuData?.remove(at: indexPath.row)
+//                self.searchMenuData = self.AllMenuData
+//                self.searchTableView.reloadData()
+//                self.search.text = ""
+//            }
+//        }
+//        return UISwipeActionsConfiguration(actions: [action])
+    }
 }
 
 extension SearchViewController : UISearchBarDelegate {
@@ -143,5 +205,14 @@ extension SearchViewController : UISearchBarDelegate {
         }
         searchTableView.reloadData()
         
+    }
+}
+
+extension SearchViewController {
+    @objc private func refreshData() {
+        searchTableView.reloadData()
+        
+        // End the refreshing animation
+        refreshControl.endRefreshing()
     }
 }
